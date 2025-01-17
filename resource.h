1ddc31d6f208f2ce4237dccee67dc6919790b9c7
DeathHound <acraig176@hotmail.com>
2025-01-17 15:30:26 +0000

    Add registered classes names' to encyclopedia

--- resource.h
+++ resource.h
@@ -1,15 +1,16 @@
 //{{NO_DEPENDENCIES}}
-// fichier Include Microsoft Visual C++.
-// Utilisé par Resource.rc
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
 //
 #define IDI_ICON1                       101
 #define IDR_DATA1                       103
+#define IDR_DATA2                       106
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
